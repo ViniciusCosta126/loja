3b657750352c423da4994772aebeb6ca0aefb4fe
viniciuscosta126 <viniciuscosta126@gmail.com>
2023-06-15 21:00:59 -0300

    feat: create input shared

--- src/modules/login/screens/Loguin.tsx
+++ src/modules/login/screens/Loguin.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { ContainerLoguin } from '../styles/login.style'
+import Input from '../../../shared/components/input/Input'
 
 const Loguin = () => {
   return (
     <View>
+      <ContainerLoguin>
         <Text>Loguin</Text>
+        <Input />
+      </ContainerLoguin>
     </View>
   )
 }
---
+++ src/shared/components/input/Input.tsx
@@ -0,0 +1,13 @@
+import { TextInputProps } from "react-native"
+import { ContainerInput } from "./input.style"
+
+interface InputProps extends TextInputProps {
+
+}
+const Input = ({ ...props }: InputProps) => {
+    return (
+        <ContainerInput {...props} />
+    )
+}
+
+export default Input
